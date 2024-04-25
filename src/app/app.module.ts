e5e2db897f2668c725c62c10aa5d48211b3d30e4
Edgar Benitez <edgarbenitez92@gmail.com>
2024-04-25 12:55:24 -0400

    Creamos diferentes componentes pages de interes y los enroutamos
    -. Home, Detail y Gallery con los nuevos componentes
    -. Instalamos un sidebar y navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { IonicModule } from '@ionic/angular';
+import { HomeComponent } from './pages/home/home.component';
+import { DetailComponent } from './pages/detail/detail.component';
+import { GalleryComponent } from './pages/gallery/gallery.component';
+import { AppRoutingModule } from './app.routes';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DetailComponent,
+    GalleryComponent
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     IonicModule.forRoot()
   ],
   providers: [],
