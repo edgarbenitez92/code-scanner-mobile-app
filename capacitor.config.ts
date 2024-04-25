eadbbcc8752be909d5b28c4d406c73653d255702
Edgar Benitez <edgarbenitez92@gmail.com>
2024-04-25 13:40:52 -0400

    Instalamos @capacitor/angular para manejo de camera
    Adicional instalamos @capacitor/camera

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.edgartesting.app',
+  appName: 'code-scanner-mobile-app',
+  webDir: 'dist/code-scanner-mobile-app'
+};
+
+export default config;
